3f07dd831cce7d6c902bc9b006634a6229d92cf1
zigiiprens <samosyzigii1@gmail.com>
2017-10-10 21:35:17 +0300

    Update lotto_test.go

--- lotto_test.go
+++ lotto_test.go
@@ -25,14 +25,14 @@ func TestLottonumber(t *testing.T) {
 			if (w > 48) {t.Error("Error with number better than 48")}
 			if (w < 0) {t.Error("Error with number less than 0")}
 			if (len(v)!=7) {t.Error("Error with lotto number lenght")}
-            if (within(float64(c),float64(w),0.02) == true) {t.Error("Error with maxDifference")}
-            c = w
+                        if (within(float64(c),float64(w),0.02) == true) {t.Error("Error with maxDifference")}
+                        c = w
 		}		
 	}
 	s := distuv.Uniform{Min : 0, Max : 49}
-    std := s.StdDev()
-    skn := s.Skewness()
-	kurt := s.ExKurtosis()
+        std := s.StdDev()
+        skn := s.Skewness()
+        kurt := s.ExKurtosis()
 	fmt.Println(std, skn, kurt)
 	fmt.Println("Total Numbers generated is %d",total)
 }
