8ed5ca7ae425669f8cac3091ac81ff7748acc90a
zigiiprens <samosyzigii1@gmail.com>
2017-10-10 21:40:02 +0300

    Update lotto_test.go

--- lotto_test.go
+++ lotto_test.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
+	"gonum.org/v1/gonum/stat/distuv"
+	"math"
 	"testing"
-        "math"
-        "gonum.org/v1/gonum/stat/distuv"
 )
-
-
 
 func within(a, b, maxDiff float64) bool {
 	absDiff := math.Abs(a - b)
@@ -15,24 +13,35 @@ func within(a, b, maxDiff float64) bool {
 }
 
 func TestLottonumber(t *testing.T) {
+
 	n := 10
 	x := lotonumber(n)
 	total := 0
 	c := 0
+
 	for _, v := range x {
-		for _, w := range v { 
-			total++                   
-			if (w > 48) {t.Error("Error with number better than 48")}
-			if (w < 0) {t.Error("Error with number less than 0")}
-			if (len(v)!=7) {t.Error("Error with lotto number lenght")}
-                        if (within(float64(c),float64(w),0.02) == true) {t.Error("Error with maxDifference")}
-                        c = w
-		}		
+		for _, w := range v {
+			total++
+			if w > 48 {
+
+				t.Error("Error with number better than 48")
+			}
+			if w < 0 {
+				t.Error("Error with number less than 0")
+			}
+			if len(v) != 7 {
+				t.Error("Error with lotto number lenght")
+			}
+			if within(float64(c), float64(w), 0.02) == true {
+				t.Error("Error with maxDifference")
+			}
+			c = w
+		}
 	}
-	s := distuv.Uniform{Min : 0, Max : 49}
-        std := s.StdDev()
-        skn := s.Skewness()
-        kurt := s.ExKurtosis()
+	s := distuv.Uniform{Min: 0, Max: 49}
+	std := s.StdDev()
+	skn := s.Skewness()
+	kurt := s.ExKurtosis()
 	fmt.Println(std, skn, kurt)
-	fmt.Println("Total Numbers generated is %d",total)
+	fmt.Println("Total Numbers generated is %d", total)
 }
